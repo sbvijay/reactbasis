3e8e5816374ce108eedd4eab6bf410a149f8173a
sbvijay <vijay@sketchbrahma.com>
2019-10-29 18:24:25 +0530

    card design with icon

--- src/components/UserView.js
+++ src/components/UserView.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-
+import {FaPhone, FaSearch,FaBuilding,FaAddressBook} from "react-icons/fa";
+import { MdLocationOn ,MdEmail,MdPerson ,MdPersonOutline,MdBookmarkBorder} from "react-icons/md";
 class UserDetails extends Component {
   constructor(props) {
     super(props);
@@ -18,28 +19,22 @@ class UserDetails extends Component {
   }
 
   render() {
+    const address = this.state.users.address;
+    const company = this.state.users.company;
+
     return (
       <div className="user-detail">
-         <h3>User Details</h3> 
+        <h3>User Details</h3>
         <ul className="list-unstyled">
-            <li>
-                ID:{this.state.users.id}
-            </li>
-            <li>
-                User Name:{this.state.users.username}
-            </li>
-            <li>
-                Name:{this.state.users.name}
-            </li>
-            <li>
-                Email:{this.state.users.email}
-            </li>
-            <li>
-                Phone:{this.state.users.phone}
-            </li>
-            <li>
-                Website:{this.state.users.website}
-            </li>
+          <li><MdBookmarkBorder/> ID: {this.state.users.id}</li>  
+          <li><MdPersonOutline/> Name: {this.state.users.name}</li>
+          <li><MdPerson/> User Name: {this.state.users.username}</li>
+          <li><MdEmail/> Email: {this.state.users.email}</li>
+          <li><FaPhone/> Phone: {this.state.users.phone}</li>
+          <li><FaSearch/> Website: {this.state.users.website}</li>
+          <li><MdLocationOn/> Address: {address ? address.street : ""}, {address ? address.suite : ""}, {address ? address.city : ""}, {address ? address.suite : ""} </li>
+          <li><FaAddressBook/> Zipcode: {address ? address.zipcode : ""} </li>
+          <li><FaBuilding/> Company: {company ? company.name : ""} </li>
         </ul>
       </div>
     );
