535823bdf125d33b4be2c5d89e9e3008c1320ba6
sbvijay <vijay@sketchbrahma.com>
2019-10-29 12:06:33 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import React,{ Component } from 'react';
-import User from "./User";
+import User from './User';
 
 class App extends Component{
-  constructor(props){
-    super(props);
-    this.state= {
-      users: [],
-    }
-  }
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          users: json,
-        })
-      });
-  }
-  userClick(message){
-    console.log(message)
-}
+  
   render() {
-    var {  users } = this.state; 
       return (
         <div className="App">
-            {users.map((user, key) => {
-              return(
-                <User key={`user-details-${key}`} user={user} onChange={this.userClick.bind(this)}/>
-              )
-            }
-            )}
+            Home
         </div>
       );
     }
--- src/UserDetails.js
+++ src/UserDetails.js
@@ -1,17 +1,34 @@
 import React,{ Component } from 'react';
  
 class UserDetails extends Component{
-    render(){
-        return(
-            <div className="user-details"> 
-            <ul className="list-unstyled">
-              <li>
-                UserDetails
-              </li>
-            </ul>
-        </div>
-        )
-    }
+  userOnClick(message){
+    // this.props.onChange(message);
+    fetch('https://jsonplaceholder.typicode.com/users/'+ message)
+    .then(res => res.json())
+    // .then(json => {
+    //   this.setState({
+    //     users: json,
+    //   })
+    // });
+    .then(data=>console.log(data))
+  }
+render(){
+    const user = this.props.user;
+   
+    return(
+      <div className="user"> 
+        <ul  className="list-unstyled">
+          <li>
+            {user.username}
+          </li> 
+          <button onClick={this.userOnClick.bind(this, user.id)}>
+                Click
+          </button>
+        </ul>
+      </div>
+      
+    );
+}
 }
 
 export default UserDetails;
